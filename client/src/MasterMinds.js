969425b622308810ff6fa7749774b2ee188356f2
Avinash Agrawal <journeymanavi@gmail.com>
2018-08-17 10:17:00 +0530

    basic routing in place

--- client/src/MasterMinds.js
+++ client/src/MasterMinds.js
@@ -10,18 +10,25 @@ const EVENT_ERROR = 'QUIZ_ERROR';
 const EVENT_UPDATE_VIEW = 'UPDATE_VIEW';
 const EVENT_REGISTER_QUIZ_MASTER = 'REGISTER_QUIZ_MASTER';
 const EVENT_CONNECT_QUIZ_DISPLAY = 'CONNECT_QUIZ_DISPLAY';
+const EVENT_QUIZ_MASTER_ACTION = 'QUIZ_MASTER_ACTION';
+
+const ACTION_SHOW_SCORES = "SHOW_SCORES";
+const ACTION_SHOW_NEXT_QUESTION = "SHOW_NEXT_QUESTION";
 
 const VIEW_WELCOME = 'WELCOME';
 const VIEW_ERROR = 'ERROR';
 const VIEW_PASSWORD = 'PASSWORD';
-const VIEW_QUIZ_MASTER = 'QUIZ_MASTER';
-const VIEW_QUIZ_DISPLAY = 'QUIZ_DISPLAY';
+const VIEW_QUIZ_MASTER_CONSOLE = 'QUIZ_MASTER_CONSOLE';
+const VIEW_QUIZ_DISPLAY_HOME = 'QUIZ_DISPLAY_HOME';
+const VIEW_QUIZ_DISPLAY_SCORES = 'QUIZ_DISPLAY_SCORES';
+const VIEW_QUIZ_DISPLAY_QUESTION = 'QUIZ_DISPLAY_QUESTION';
 
 class MasterMinds extends Component {
   constructor() {
     super();
     this.state = {
-      view: VIEW_WELCOME
+      password: 'passw0rd',
+      view: { name: VIEW_WELCOME }
     }
   }
 
@@ -37,7 +44,7 @@ class MasterMinds extends Component {
   }
 
   showPassword = () => {
-    this.setState({ view: VIEW_PASSWORD});
+    this.setState({ view: { name: VIEW_PASSWORD } });
   };
 
   handlePasswordChange = (event) => {
@@ -46,19 +53,37 @@ class MasterMinds extends Component {
     });
   };
 
-  registerQuizMaster = () => {
-    this.socket.emit(EVENT_REGISTER_QUIZ_MASTER, { password: this.state.password });
+  registerQuizMaster = (event) => {
+    this.socket.emit(
+      EVENT_REGISTER_QUIZ_MASTER,
+      { password: this.state.password }
+    );
+    event.preventDefault();
   }
 
   connectDisplay = () => {
-    this.socket.emit(EVENT_CONNECT_QUIZ_DISPLAY)
+    this.socket.emit(EVENT_CONNECT_QUIZ_DISPLAY);
   }
+
+  showScores = () => {
+    this.socket.emit(
+      EVENT_QUIZ_MASTER_ACTION,
+      { name: ACTION_SHOW_SCORES }
+    );
+  };
+
+  showNextQuestion = () => {
+    this.socket.emit(
+      EVENT_QUIZ_MASTER_ACTION,
+      { name: ACTION_SHOW_NEXT_QUESTION }
+    );
+  };
 
   render() {
     const { view } = this.state;
 
     let viewToRender = null;
-    switch(view) {
+    switch(view.name) {
       case VIEW_ERROR:
       viewToRender = <React.Fragment>
         <div>{this.state.error}</div>
@@ -83,23 +108,44 @@ class MasterMinds extends Component {
       viewToRender = <React.Fragment>
         <Logo />
         <div className='quiz-master-password'>
-          <input
-            type='password'
-            value={this.state.password}
-            onChange={this.handlePasswordChange}
-          />
-          <button onClick={this.registerQuizMaster}>Start Quiz</button>
+          <form onSubmit={this.registerQuizMaster}>
+            <input
+              type='password'
+              value={this.state.password}
+              onChange={this.handlePasswordChange}
+            />
+            <button type='submit'>Start Quiz</button>
+          </form>
         </div>
       </React.Fragment>;
       break;
 
-      case VIEW_QUIZ_MASTER:
-      viewToRender = <div>Quiz Master Home</div>
+      case VIEW_QUIZ_MASTER_CONSOLE:
+      viewToRender = <React.Fragment>
+        Quiz Master Console
+        <button onClick={this.showScores}>Show Scores</button>
+        <button onClick={this.showNextQuestion}>Ask Question</button>
+      </React.Fragment>
       break;
 
-      case VIEW_QUIZ_DISPLAY:
-      viewToRender = <div>Quiz Display</div>
+      case VIEW_QUIZ_DISPLAY_HOME:
+      viewToRender = <div>Quiz Display Home</div>
       break;
+
+      case VIEW_QUIZ_DISPLAY_SCORES:
+      viewToRender = <div>Scores</div>
+      break;
+
+      case VIEW_QUIZ_DISPLAY_QUESTION:
+      viewToRender = <React.Fragment>
+        <div>Question</div>
+        <div>{view.question.text}</div>
+        <div>Question</div>
+      </React.Fragment>
+      break;
+
+      default:
+      console.log(`Can't recognize view name: ${view.name}`);
     }
 
     return (
--- server/index.js
+++ server/index.js
@@ -18,9 +18,15 @@ const EVENT_ERROR = 'QUIZ_ERROR';
 const EVENT_UPDATE_VIEW = 'UPDATE_VIEW';
 const EVENT_REGISTER_QUIZ_MASTER = 'REGISTER_QUIZ_MASTER';
 const EVENT_CONNECT_QUIZ_DISPLAY = 'CONNECT_QUIZ_DISPLAY';
+const EVENT_QUIZ_MASTER_ACTION = 'QUIZ_MASTER_ACTION';
 
-const VIEW_QUIZ_MASTER = 'QUIZ_MASTER';
-const VIEW_QUIZ_DISPLAY = 'QUIZ_DISPLAY';
+const ACTION_SHOW_SCORES = "SHOW_SCORES";
+const ACTION_SHOW_NEXT_QUESTION = "SHOW_NEXT_QUESTION";
+
+const VIEW_QUIZ_MASTER_CONSOLE = 'QUIZ_MASTER_CONSOLE';
+const VIEW_QUIZ_DISPLAY_HOME = 'QUIZ_DISPLAY_HOME';
+const VIEW_QUIZ_DISPLAY_SCORES = 'QUIZ_DISPLAY_SCORES';
+const VIEW_QUIZ_DISPLAY_QUESTION = 'QUIZ_DISPLAY_QUESTION';
 
 const quizState = {
   quizMasterLoggedIn: false,
@@ -30,12 +36,40 @@ const quizState = {
 };
 
 const registerQuizMaster = (password, socket) => {
-  console.log(`registerQuizMaster with password: ${password}`);
+  console.log('Registering Quiz Master');
   if (!quizState.quizMasterLoggedIn) {
     if (password === 'passw0rd') {
       quizState.quizMasterLoggedIn = true;
       quizState.quizMasterSocket = socket.id;
-      socket.emit(EVENT_UPDATE_VIEW, VIEW_QUIZ_MASTER);
+
+      socket.on(EVENT_QUIZ_MASTER_ACTION, action => {
+        console.log(`Received Quiz Master Action: ${action}`);
+        switch(action) {
+          case ACTION_SHOW_SCORES:
+          quizState.quizDisplaySocket.emit(
+            EVENT_UPDATE_VIEW,
+            { name: VIEW_QUIZ_DISPLAY_SCORES }
+          );
+          break;
+
+          case ACTION_SHOW_NEXT_QUESTION:
+          quizState.quizDisplaySocket.emit(
+            EVENT_UPDATE_VIEW,
+            {
+              name: VIEW_QUIZ_DISPLAY_QUESTION,
+              question: {
+                text: `Some Question - ${Date.now()}`
+              }
+            }
+          );
+          break;
+        }
+      });
+
+      socket.emit(
+        EVENT_UPDATE_VIEW,
+        { name: VIEW_QUIZ_MASTER_CONSOLE }
+      );
     }
     else {
       socket.emit(EVENT_ERROR, 'Incorrect Quiz Master password.');
@@ -49,7 +83,10 @@ const registerQuizMaster = (password, socket) => {
 const connectDisplay = socket => {
   if (!quizState.quizDisplayConnected) {
     quizState.quizDisplaySocket = socket.id;
-    socket.emit(EVENT_UPDATE_VIEW, VIEW_QUIZ_DISPLAY);
+    socket.emit(
+      EVENT_UPDATE_VIEW,
+      { name: VIEW_QUIZ_DISPLAY_HOME }
+    );
   }
   else {
     socket.emit(EVENT_ERROR, 'Quiz Display already joined.');
@@ -79,11 +116,6 @@ const handleDisconnection = socket => {
  */
 webSocketServer.on('connection', socket => {
   console.log('New client connected');
-
-  // setInterval(
-  //   () => socket.emit('view', `view-${Date.now()}`),
-  //   5000
-  // );
 
   socket.on(
     EVENT_REGISTER_QUIZ_MASTER,
